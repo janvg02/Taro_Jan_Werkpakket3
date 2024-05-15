734df25019821f41c09b5e1a5f389ca20bc0dc86
janvg02 <jan.vangalen@student.pxl.be>
2024-05-15 15:15:57 +0200

    probleem image opgelost

--- config/general.php
+++ config/general.php
@@ -23,5 +23,6 @@ return GeneralConfig::create()
     // Set the @webroot alias so the clear-caches command knows where to find CP resources
     ->aliases([
         '@webroot' => dirname(__DIR__) . '/web',
+        "baseUrl" => App::env("BASE_URL"),
     ])
 ;
